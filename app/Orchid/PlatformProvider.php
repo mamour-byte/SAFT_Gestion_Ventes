78da6696daee4e34dd9493442cc4274b346edfaa
Mamour fall <mamourf958@gmail.com>
2025-03-19 06:28:41 +0000

    first commit

---
+++ app/Orchid/PlatformProvider.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Orchid;
+
+use Orchid\Platform\Dashboard;
+use Orchid\Platform\ItemPermission;
+use Orchid\Platform\OrchidServiceProvider;
+use Orchid\Screen\Actions\Menu;
+use Orchid\Support\Color;
+
+class PlatformProvider extends OrchidServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @param Dashboard $dashboard
+     *
+     * @return void
+     */
+    public function boot(Dashboard $dashboard): void
+    {
+        parent::boot($dashboard);
+
+        // ...
+    }
+
+    /**
+     * Register the application menu.
+     *
+     * @return Menu[]
+     */
+    public function menu(): array
+    {
+        return [
+
+            Menu::make('Accueil')
+                ->icon('bi-house')
+                ->title('Navigation')
+                ->route('platform.main'),
+
+            Menu::make('Product')
+                ->icon('bi-bag-fill')
+                ->route('platform.Product'),
+
+            Menu::make('Clients')
+                ->icon('bi-people')
+                ->route('platform.Clients'),
+
+            Menu::make(__('Users'))
+                ->icon('bs.people')
+                ->route('platform.systems.users')
+                ->permission('platform.systems.users')
+                ->title(__('Access Controls')),
+
+            
+        ];
+    }
+
+    /**
+     * Register permissions for the application.
+     *
+     * @return ItemPermission[]
+     */
+    public function permissions(): array
+    {
+        return [
+            ItemPermission::group(__('System'))
+                ->addPermission('platform.systems.roles', __('Roles'))
+                ->addPermission('platform.systems.users', __('Users')),
+        ];
+    }
+}
---
+++ routes/platform.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Orchid\Screens\PlatformScreen;
+use App\Orchid\Screens\Role\RoleEditScreen;
+use App\Orchid\Screens\Role\RoleListScreen;
+use App\Orchid\Screens\User\UserEditScreen;
+use App\Orchid\Screens\User\UserListScreen;
+use App\Orchid\Screens\User\UserProfileScreen;
+use Illuminate\Support\Facades\Route;
+use Tabuna\Breadcrumbs\Trail;
+
+use App\Orchid\Screens\ProductScreen;
+use App\Orchid\Screens\ClientsScreen;
+
+/*
+|--------------------------------------------------------------------------
+| Dashboard Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the need "dashboard" middleware group. Now create something great!
+|
+*/
+// Accueil
+Route::screen('/main', PlatformScreen::class)
+    ->name('platform.main');
+
+// Produit
+Route::screen('/product', ProductScreen::class)
+    ->name('platform.Product');
+
+// Clients
+Route::screen('/clients', ClientsScreen::class)
+    ->name('platform.Clients');
+
+
+
+// Platform > Profile
+Route::screen('profile', UserProfileScreen::class)
+    ->name('platform.profile')
+    ->breadcrumbs(fn (Trail $trail) => $trail
+        ->parent('platform.index')
+        ->push(__('Profile'), route('platform.profile')));
+
+// Platform > System > Users > User
+Route::screen('users/{user}/edit', UserEditScreen::class)
+    ->name('platform.systems.users.edit')
+    ->breadcrumbs(fn (Trail $trail, $user) => $trail
+        ->parent('platform.systems.users')
+        ->push($user->name, route('platform.systems.users.edit', $user)));
+
+// Platform > System > Users > Create
+Route::screen('users/create', UserEditScreen::class)
+    ->name('platform.systems.users.create')
+    ->breadcrumbs(fn (Trail $trail) => $trail
+        ->parent('platform.systems.users')
+        ->push(__('Create'), route('platform.systems.users.create')));
+
+// Platform > System > Users
+Route::screen('users', UserListScreen::class)
+    ->name('platform.systems.users')
+    ->breadcrumbs(fn (Trail $trail) => $trail
+        ->parent('platform.index')
+        ->push(__('Users'), route('platform.systems.users')));
+
+
