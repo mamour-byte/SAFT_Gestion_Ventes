bfede88bfcebd4c422e504c7f7ed39ec531d8189
Mamour <mamourf958@gmail.com>
2025-04-22 23:32:15 +0000

    update

---
+++ app/Orchid/Layouts/ChartsLayout.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Orchid\Layouts;
+
+use Orchid\Screen\Layouts\Chart;
+
+class ChartsLayout extends Chart
+{
+    /**
+     * Available options:
+     * 'bar', 'line',
+     * 'pie', 'percentage'.
+     *
+     * @var string
+     */
+    protected $type = 'bar';
+
+    /**
+     * Determines whether to display the export button.
+     *
+     * @var bool
+     */
+    protected $export = true;
+}
